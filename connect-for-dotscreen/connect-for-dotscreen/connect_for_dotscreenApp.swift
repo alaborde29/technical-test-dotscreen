510124575bbbf4854b2af858279d144c4244409b
alaborde29 <laborde.@live.fr>
2024-05-24 16:20:04 +0900

    project init

---
+++ connect-for-dotscreen/connect-for-dotscreen/connect_for_dotscreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  connect_for_dotscreenApp.swift
+//  connect-for-dotscreen
+//
+//  Created by Alexandre Laborde on 24/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct connect_for_dotscreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
